8f70899c0f91cc985a79c3807937cdea6a7a50ae
Imngzx <zhixuanng14@gmail.com>
2025-09-16 18:07:30 +0800

    added new features to coderunner

--- lua/plugins/coderunner.lua
+++ lua/plugins/coderunner.lua
@@ -1,4 +1,4 @@
-_G.RUNNER_MODE = 1 --NOTE: toggle here to switch between float or newtab mode
+_G.RUNNER_MODE = 1
 
 -- helper function to get mode
 local function get_mode()
@@ -13,13 +13,54 @@ local function get_mode()
   end
 end
 
+-- detect OS
+local is_windows = vim.loop.os_uname().sysname:match("Windows")
+-- alternative: local is_windows = vim.fn.has("win32") == 1
+
+-- compiler configs
+local filetype = {}
+
+if is_windows then
+  filetype = {
+    cpp = {
+      "cd $dir && cl /utf-8 /nologo /EHsc /O2 /std:c++latest /Zc:__cplusplus $fileName /Fe:$fileNameWithoutExt.exe && $fileNameWithoutExt.exe",
+    },
+    c = {
+      "cd $dir && cl /utf-8 /nologo /O2 $fileName /Fe:$fileNameWithoutExt.exe && $fileNameWithoutExt.exe",
+    },
+    python = {
+      "cd $dir &&",
+      "python -u $fileName",
+    },
+  }
+else
+  filetype = {
+    c = {
+      "cd $dir &&",
+      "mkdir -p out &&",
+      "gcc -Wall -Wextra -O2 -o out/$fileNameWithoutExt $fileName -lm &&",
+      "./out/$fileNameWithoutExt",
+    },
+    cpp = {
+      "cd $dir &&",
+      "mkdir -p out &&",
+      "g++ -std=c++23 -Wall -Wextra -O2 -o out/$fileNameWithoutExt $fileName -lm &&",
+      "./out/$fileNameWithoutExt",
+    },
+    python = {
+      "cd $dir &&",
+      "python3 -u $fileName",
+    },
+  }
+end
+
 return {
   "CRAG666/code_runner.nvim",
   main = "code_runner",
   cmd = { "RunCode", "RunFile", "RunProject", "RunClose" },
   opts = function()
     return {
-      mode = get_mode(), -- toggle mode
+      mode = get_mode(),
       focus = true,
       startinsert = true,
       term = { position = "bot", size = 20 },
@@ -33,24 +74,7 @@ return {
         float_hl = "Normal",
         blend = 0,
       },
-      filetype = {
-        c = {
-          "cd $dir &&",
-          "mkdir -p out &&",
-          "gcc -Wall -Wextra -O2 -o out/$fileNameWithoutExt $fileName -lm &&",
-          "./out/$fileNameWithoutExt",
-        },
-        cpp = {
-          "cd $dir &&",
-          "mkdir -p out &&",
-          "g++ -std=c++23 -Wall -Wextra -O2 -o out/$fileNameWithoutExt $fileName -lm &&",
-          "./out/$fileNameWithoutExt",
-        },
-        python = {
-          "cd $dir &&",
-          "python3 -u $fileName",
-        },
-      },
+      filetype = filetype,
     }
   end,
   keys = {
